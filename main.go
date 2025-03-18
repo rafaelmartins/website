631b72a2e83ac392a669ffc7e173585c6c7a6e29
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2025-03-18 21:46:49 +0100

    main: upgrade nunito

--- main.go
+++ main.go
@@ -91,7 +91,7 @@ func getTaskGroups(c *config.Config) ([]*runner.TaskGroup, error) {
 		runner.NewTaskGroup(
 			&tasks.NpmPackage{
 				Name:    "@fontsource-variable/nunito",
-				Version: "5.1.1",
+				Version: "5.2.5",
 				Files: []string{
 					"wght.min.css",
 					"files/nunito-cyrillic-ext-wght-normal.woff2",
