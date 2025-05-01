e6ea79c789251de869600a188cbd1ed97e6405b3
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2025-05-01 02:01:23 +0200

    meta: use version generated by go compiler

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"runtime/debug"
 	"strconv"
-	"strings"
-	"time"
 )
+
+var baseUrl = "https://github.com/rafaelmartins/website"
 
 type MetadataGo struct {
 	Version string
@@ -37,8 +37,6 @@ type Metadata struct {
 
 	Git struct {
 		Revision string
-		Date     time.Time
-		Dirty    bool
 		URL      string
 	}
 
@@ -75,20 +73,6 @@ func GetMetadata() (*Metadata, error) {
 		case "vcs.revision":
 			rv.Git.Revision = s.Value
 
-		case "vcs.time":
-			t, err := time.Parse(time.RFC3339, s.Value)
-			if err != nil {
-				return nil, err
-			}
-			rv.Git.Date = t
-
-		case "vcs.modified":
-			m, err := strconv.ParseBool(s.Value)
-			if err != nil {
-				return nil, err
-			}
-			rv.Git.Dirty = m
-
 		case "CGO_ENABLED":
 			m, err := strconv.ParseBool(s.Value)
 			if err != nil {
@@ -107,27 +91,11 @@ func GetMetadata() (*Metadata, error) {
 	if !isGit {
 		// assume that we are on a different worktree during development
 		rv.Git.Revision = "0000000"
-		rv.Git.Date = time.Now().UTC()
-	}
-
-	if len(rv.Git.Revision) < 7 || rv.Git.Date.IsZero() {
-		return nil, errors.New("meta: required git metadata not found")
-	}
-	if !strings.HasPrefix(bi.Path, "github.com/") {
-		return nil, errors.New("meta: module not from github, unsupported")
-	}
-
-	pathp := strings.Split(bi.Path, "/")
-	if len(pathp) < 2 {
-		return nil, errors.New("meta: bad module format")
 	}
 
 	rv.Name = bi.Path
-	rv.Version = rv.Git.Date.Format("2006010215") + "-" + rv.Git.Revision[:7]
-	if rv.Git.Dirty {
-		rv.Version += "-dirty"
-	}
-	rv.URL = fmt.Sprintf("https://%s/%s/%s", pathp[0], pathp[1], pathp[2])
+	rv.Version = bi.Main.Version
+	rv.URL = baseUrl
 	rv.Git.URL = fmt.Sprintf("%s/commit/%s", rv.URL, rv.Git.Revision)
 	rv.Go.Version = bi.GoVersion
 
