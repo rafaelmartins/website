6ed0b3075638eca1b2db025b4f82dad537532850
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2025-05-17 17:13:16 +0200

    cdocs: minor optimizations

--- internal/cdocs/highlight.go
+++ internal/cdocs/highlight.go
@@ -9,22 +9,25 @@ import (
 	"github.com/alecthomas/chroma/v2/styles"
 )
 
+var (
+	chFormatter = html.New(
+		html.WithClasses(false),
+		html.WithLineNumbers(false),
+	)
+	chLexer = chroma.Coalesce(lexers.Get("c"))
+	chStyle = styles.Get("github")
+)
+
 func highlight(code string) (string, error) {
-	iter, err := chroma.Coalesce(lexers.Get("c")).Tokenise(nil, code)
+	iter, err := chLexer.Tokenise(nil, code)
 	if err != nil {
 		return "", err
 	}
 
-	formatter := html.New(
-		html.WithClasses(false),
-		html.WithLineNumbers(false),
-	)
-
 	// we use github style to match the project pages, that are rendered by github api
 	b := bytes.Buffer{}
-	if err := formatter.Format(&b, styles.Get("github"), iter); err != nil {
+	if err := chFormatter.Format(&b, chStyle, iter); err != nil {
 		return "", err
 	}
-
 	return b.String(), nil
 }
