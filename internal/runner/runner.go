720f0149a9f6779d148d244e05452a00cc409834
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2025-10-18 23:21:04 +0200

    runner: improve logging

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -54,12 +54,12 @@ func (t *Task) destination(basedir string) string {
 	return filepath.Join(basedir, t.group.GetBaseDestination(), t.impl.GetDestination())
 }
 
-func (t *Task) run(basedir string, cfg Config, force bool) error {
+func (t *Task) run(basedir string, cfg Config, force bool) (bool, error) {
 	if t.impl == nil {
-		return errors.New("task missing implementation")
+		return false, errors.New("task missing implementation")
 	}
 	if t.group == nil {
-		return errors.New("task group is nil")
+		return false, errors.New("task group is nil")
 	}
 
 	var gen Generator
@@ -67,7 +67,7 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 		var err error
 		gen, err = t.impl.GetGenerator()
 		if err != nil {
-			return err
+			return false, err
 		}
 		t.gen = gen
 	} else {
@@ -78,13 +78,13 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 	if !force {
 		timestamps, err := gen.GetTimeStamps()
 		if err != nil {
-			return err
+			return false, err
 		}
 
 		if cfg != nil && !gen.GetImmutable() {
 			ctimestamp, err := cfg.GetTimeStamp()
 			if err != nil {
-				return err
+				return false, err
 			}
 			timestamps = append(timestamps, ctimestamp)
 		}
@@ -94,7 +94,7 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 			if !slices.ContainsFunc(timestamps, func(ts time.Time) bool {
 				return ts.Compare(destts) > 0
 			}) {
-				return nil
+				return false, nil
 			}
 		}
 	}
@@ -103,11 +103,11 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 
 	rd, err := gen.GetReader()
 	if err != nil {
-		return err
+		return true, err
 	}
 
 	if err := postproc.PostProc(dest, rd); err != nil {
-		return err
+		return true, err
 	}
 
 	ch := make(chan *GeneratorByProduct)
@@ -123,7 +123,7 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 
 	for bp := range ch {
 		if bp.Err != nil {
-			return bp.Err
+			return true, bp.Err
 		}
 
 		bpDest := filepath.Join(bpDir, bp.Filename)
@@ -131,11 +131,11 @@ func (t *Task) run(basedir string, cfg Config, force bool) error {
 		log.Printf("  %-8s  %s [%s]", gen.GetID(), dest, bpDest)
 
 		if err := postproc.PostProc(bpDest, bp.Reader); err != nil {
-			return err
+			return true, err
 		}
 	}
 
-	return nil
+	return true, nil
 }
 
 type TaskGroupImpl interface {
@@ -187,6 +187,7 @@ func Run(groups []*TaskGroup, basedir string, cfg Config, force bool) error {
 	nworkers := int64(runtime.NumCPU())
 	sem := semaphore.NewWeighted(nworkers)
 	failures := atomic.Int32{}
+	outdated := atomic.Int32{}
 
 	for _, t := range allTasks {
 		if err := sem.Acquire(ctx, 1); err != nil {
@@ -196,9 +197,11 @@ func Run(groups []*TaskGroup, basedir string, cfg Config, force bool) error {
 		go func(task *Task) {
 			defer sem.Release(1)
 
-			if err := task.run(basedir, cfg, force); err != nil {
+			if outd, err := task.run(basedir, cfg, force); err != nil {
 				failures.Add(1)
 				log.Printf("  %-8s  %s: %s", "[ERROR]", task.destination(basedir), err)
+			} else if outd {
+				outdated.Add(1)
 			}
 		}(t)
 	}
@@ -210,5 +213,10 @@ func Run(groups []*TaskGroup, basedir string, cfg Config, force bool) error {
 	if f := failures.Load(); f > 0 {
 		return fmt.Errorf("runner: %d tasks failed", f)
 	}
+
+	if outdated.Load() > 0 {
+		log.Printf("--------------------------------------------------------------------------------")
+	}
+
 	return nil
 }
