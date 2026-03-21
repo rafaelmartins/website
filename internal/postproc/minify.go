6166e2951e2ad35c6bfe8c20ce2d3fc92d232c50
Rafael G. Martins <rafael@rafaelmartins.eng.br>
2026-03-21 23:38:44 +0100

    postproc: re-enable html minification
    it was disabled because it was breaking go vanity imports. just found out
    that this was being caused by the removal of quotes.

--- internal/postproc/minify.go
+++ internal/postproc/minify.go
@@ -25,7 +25,7 @@ var mimeTypes = []*mimeType{
 	{"application/json", []string{".json"}},
 	{"image/svg+xml", []string{".svg"}},
 	{"text/css", []string{".css"}},
-	// {"text/html", []string{".html", ".htm"}},
+	{"text/html", []string{".html", ".htm"}},
 	{"text/javascript", []string{".js", ".jsm"}},
 	{"text/xml", []string{".xml", ".xsl", ".rss", ".xslt", ".xsd", ".wsdl", ".wsf", ".atom"}},
 }
@@ -52,27 +52,21 @@ func (m *Minify) Run(dstFn string, dst io.Writer, src io.Reader) error {
 		m.mi = minify.New()
 
 		m.mi.Add("application/json", &json.Minifier{})
-		m.mi.Add("image/svg+xml", &svg.Minifier{
-			KeepComments: false,
-			Precision:    0,
-		})
+		m.mi.Add("image/svg+xml", &svg.Minifier{})
 		m.mi.Add("text/css", &css.Minifier{
-			Precision: 0,
-			Version:   2,
+			Version: 3,
 		})
 		m.mi.Add("text/html", &html.Minifier{
-			KeepDocumentTags:    true,
 			KeepSpecialComments: true,
-			KeepEndTags:         true,
 			KeepDefaultAttrVals: true,
-			KeepWhitespace:      false,
+			KeepDocumentTags:    true,
+			KeepEndTags:         true,
+			KeepQuotes:          true,
 		})
 		m.mi.Add("text/javascript", &js.Minifier{
 			Version: 2022,
 		})
-		m.mi.Add("text/xml", &xml.Minifier{
-			KeepWhitespace: false,
-		})
+		m.mi.Add("text/xml", &xml.Minifier{})
 	}
 
 	ext := filepath.Ext(dstFn)
